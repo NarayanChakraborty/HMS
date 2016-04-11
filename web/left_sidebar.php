9b4bb6dc3d7aac173aeb331b8abc016def2f3ad0
SN Chakraborty <SN Chakraborty>
2016-04-11 23:52:24 +0600

    Front-End Development

---
+++ web/left_sidebar.php
@@ -0,0 +1,16 @@
+	
+	<ul>
+		    <li><a href="entry_patient.php" class="btn btn-secondary btn-lg active" id="button" role="button">Entry Patient</a></li>
+			<li><a href="entry_doctor.php" class="btn btn-secondary btn-lg active" id="button" role="button">Entry Doctor</a></li>
+			<li><a href="entry_nurse.php" class="btn btn-secondary btn-lg active" id="button" role="button">Entry Nurse</a></li>
+			<li><a href="release_patient.php" class="btn btn-secondary btn-lg active" id="button" role="button">Release Patient</a></li>
+		    <li class="dropdown"> <a href="#"  id="button" class="dropdown-toggle btn btn-secondary btn-lg active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Query Information<span class="caret"></span></a> 
+												<ul class="dropdown-menu" aria-labelledby="navbarDrop1">
+												<li class=""><a href="patient_details.php">Patient</a></li> 
+													<li class=""><a href="doctor_details.php">Doctor</a></li> 
+													<li class=""><a href="nurse_details.php">Nurse</a></li> 
+													<li class=""><a href="receptionist.php">Receiptionist</a></li> 
+													<li class=""><a href="room_details.php">Rooms</a></li>
+									</ul> 
+			</li> 
+		 </ul>
