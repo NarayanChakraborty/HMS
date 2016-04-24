4a2a8a39a5019d016344de76b1b23dfa98f7db53
NarayanChakraborty <narayanchakraborty1993@gmail.com>
2016-04-24 17:54:40 +0600

    Developing Database Connection

--- doctors_view.php
+++ doctors_view.php
@@ -16,51 +16,68 @@ header('location: index.php');
 	<!--banner-->
 	<div class="row">
         
-        <div class="col-xs-6 col-md-4">
+        <div class="col-xs-6 col-md-3">
 		   
 		  		   <?php 
 		   include("left_sidebar.php");
 		   ?>
 		
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-8"><h1 style="text-align:center;margin-left:-130px;margin-top:100px;padding-bottom:200px;">
-		
-										<div class="col-sm-4 box">
-							
-											<img class= "image" src="images/d1.jpg" alt="Megla Das"/>
-											<h2>Megla Das</h2>
-											<p>MSC in Cardiology</p>
-						
-										</div>
-										<div class="col-sm-4 box">
-							
-											<img class= "image" src="images/d2.png" alt="Megla Das"/>
-											<h2>Megla Das</h2>
-											<p>MSC in Cardiology</p>
-						
-										</div>
-										<div class="col-sm-4 box">
-							
-											<img class= "image" src="images/d3.jpg" alt="Megla Das"/>
-											<h2>Megla Das</h2>
-											<p>MSC in Cardiology</p>
-						
-										</div>
-										<div class="col-sm-4 box">
-							
-											<img class= "image" src="images/d4.jpg" alt="Megla Das"/>
-											<h2>Megla Das</h2>
-											<p>MSC in Cardiology</p>
-						
-										</div>
-										<div class="col-sm-4 box">
-							
-											<img class= "image" src="images/d5.jpg" alt="Megla Das"/>
-											<h2>Megla Das</h2>
-											<p>MSC in Cardiology</p>
-						
-										</div>
-                     
+		<div class="col-xs-12 col-sm-8 col-md-9">
+                    
+                   	<!--about-team-->
+	<div class="about-team"> 
+		<div class="container">
+			<h3 class="title1">Meet Our <span>Doctors</span></h3>
+			<div class="team-info">
+				<div class="col-md-4 about-team-grids">
+					<img src="images/d1.jpg" alt=""/>
+					<div class="team-text">
+						<h4><span>ANDERSON,</span> Surgeon</h4>
+						<p>Odio dignissimos vero eos voluptatem accusantium doloremque laudantium reader will be distracted.</p>
+					</div>
+					<div class="caption">
+						<ul>
+							<li><a href="#"></a></li>
+							<li><a href="#" class="f2"></a></li>
+							<li><a href="#" class="f3"></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class=" col-md-4 about-team-grids">
+					<img src="images/d4.jpg" alt=""/>
+					<div class="team-text">
+						<h4><span>JESSICA,</span>Director</h4>
+						<p>Odio dignissimos vero eos voluptatem accusantium doloremque laudantium reader will be distracted.</p>
+					</div>
+					<div class="caption">
+						<ul>
+							<li><a href="#"></a></li>
+							<li><a href="#" class="f2"></a></li>
+							<li><a href="#" class="f3"></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 about-team-grids">
+					<img src="images/d3.jpg" alt=""/>
+					<div class="team-text">
+						<h4><span>ELIFS</span>Cardiologists</h4>				
+						<p>Odio dignissimos vero eos voluptatem accusantium doloremque laudantium reader will be distracted.</p>
+					</div>
+					<div class="caption">
+						<ul>
+							<li><a href="#"></a></li>
+							<li><a href="#" class="f2"></a></li>
+							<li><a href="#" class="f3"></a></li>
+						</ul>
+					</div>
+				</div>
+				
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//about-team-->				
 		
 		</div>
      </div>
